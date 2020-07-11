5a606611de423a9d50ccd1e420386ffcae1e4af2
iamvatsal <vatsalkesarwani12@gmail.com>
2020-07-11 17:40:09 +0530

    twitter , yahoo , anonymous sign in added. Last Commit..😉

--- app/src/main/java/com/vatsal/kesarwani/testassignment/Welcome.java
+++ app/src/main/java/com/vatsal/kesarwani/testassignment/Welcome.java
@@ -63,7 +63,7 @@ public class Welcome extends AppCompatActivity {
     //private ImageButton facebook;
     //private SignInButton google;
     private LoginButton facebook;
-    private Button email, github,google,phone,play,twitter,yahoo,anonymous;
+    private Button email, github,google,phone,twitter,yahoo,anonymous;
     //private TextView login;
     private GoogleSignInClient mGoogleSignInClient;
     private CallbackManager callbackManager;
@@ -76,24 +76,6 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_activity);
         initializeView();
-
-        final OAuthProvider.Builder provider = OAuthProvider.newBuilder("github.com");
-        // Request read access to a user's email addresses.
-        // This must be preconfigured in the app's API permissions.
-        List<String> scopes =
-                new ArrayList<String>() {
-                    {
-                        add("user:email");
-                    }
-                };
-        provider.setScopes(scopes);
-
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         email.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,13 +91,42 @@ public class Welcome extends AppCompatActivity {
             }
         });
 
-        google.setOnClickListener(new View.OnClickListener() {
+        anonymous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signIn();
+                anonSignin();
             }
         });
 
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                twitterSignin();
+            }
+        });
+
+        yahoo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                yahooSignin();
+            }
+        });
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googlesignIn();
+            }
+        });
+
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                githubSignIn();
+            }
+        });
+
+        /**facebook*/
         facebook.setReadPermissions(Arrays.asList(EMAIL));
 
         facebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -146,50 +157,143 @@ public class Welcome extends AppCompatActivity {
                 }
             }
         };
+        /**facebook*/
+
+    }
+
+    private void githubSignIn() {
+
+        OAuthProvider.Builder provider = OAuthProvider.newBuilder("github.com");
+        // Request read access to a user's email addresses.
+        // This must be preconfigured in the app's API permissions.
+        List<String> scopes =
+                new ArrayList<String>() {
+                    {
+                        add("user:email");
+                    }
+                };
+        provider.setScopes(scopes);
+
+        mAuth.startActivityForSignInWithProvider(/* activity= */ Welcome.this, provider.build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                // User is signed in.
+                                // IdP data available in
+                                // authResult.getAdditionalUserInfo().getProfile().
+                                // The OAuth access token can also be retrieved:
+                                //  authResult.getCredential().getAccessToken().
+                                Log.d("git token",authResult.getUser().getDisplayName());
+                                //Log.d("git cred",authResult.getCredential().toString());
+                                //Log.d("access token",((OAuthCredential)authResult.getCredential()).getAccessToken());
 
 
-        github.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                                Toast.makeText(Welcome.this, "Github Sign in successful", Toast.LENGTH_SHORT).show();
+                                Log.d("git login","success");
+                                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Handle failure.
+                                Toast.makeText(Welcome.this, "Github Sign in failed", Toast.LENGTH_SHORT).show();
+                                Log.d("git login","failed"+ e.getMessage());
+                            }
+                        });
 
-                mAuth.startActivityForSignInWithProvider(/* activity= */ Welcome.this, provider.build())
-                        .addOnSuccessListener(
-                                new OnSuccessListener<AuthResult>() {
-                                    @Override
-                                    public void onSuccess(AuthResult authResult) {
-                                        // User is signed in.
-                                        // IdP data available in
-                                        // authResult.getAdditionalUserInfo().getProfile().
-                                        // The OAuth access token can also be retrieved:
-                                        //  authResult.getCredential().getAccessToken().
-                                        Log.d("git token",authResult.getUser().getDisplayName());
-                                        //Log.d("git cred",authResult.getCredential().toString());
-                                        //Log.d("access token",((OAuthCredential)authResult.getCredential()).getAccessToken());
-
-
-                                        Toast.makeText(Welcome.this, "Github Sign in successful", Toast.LENGTH_SHORT).show();
-                                        Log.d("git login","success");
-                                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-                                    }
-                                })
-                        .addOnFailureListener(
-                                new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        // Handle failure.
-                                        Toast.makeText(Welcome.this, "Github Sign in failed", Toast.LENGTH_SHORT).show();
-                                        Log.d("git login","failed"+ e.getMessage());
-                                    }
-                                });
-
-            }
-            //}
-        });
     }
 
 
-    AccessToken accessToken = AccessToken.getCurrentAccessToken();
-    boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+    private void yahooSignin() {
+
+        OAuthProvider.Builder provider = OAuthProvider.newBuilder("yahoo.com");
+
+        mAuth
+                .startActivityForSignInWithProvider(/* activity= */ this, provider.build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                // User is signed in.
+                                // IdP data available in
+                                // authResult.getAdditionalUserInfo().getProfile().
+                                // The OAuth access token can be retrieved:
+                                // authResult.getCredential().getAccessToken().
+                                // Yahoo OAuth ID token can also be retrieved:
+                                // authResult.getCredential().getIdToken().
+                                Toast.makeText(Welcome.this, ""+authResult.getUser().getDisplayName(), Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Handle failure.
+                            }
+                        });
+
+    }
+
+    private void twitterSignin() {
+        OAuthProvider.Builder provider = OAuthProvider.newBuilder("twitter.com");
+
+        mAuth
+                .startActivityForSignInWithProvider(/* activity= */ this, provider.build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                // User is signed in.
+                                // IdP data available in
+                                // authResult.getAdditionalUserInfo().getProfile().
+                                // The OAuth access token can also be retrieved:
+                                // authResult.getCredential().getAccessToken().
+                                // The OAuth secret can be retrieved by calling:
+                                // authResult.getCredential().getSecret().
+                                Toast.makeText(Welcome.this, ""+authResult.getUser().getDisplayName(), Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Handle failure.
+                                Toast.makeText(Welcome.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+    }
+
+    private void anonSignin() {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("anonymous", "signInAnonymously:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(Welcome.this, ""+user.getDisplayName(), Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("anonymous", "signInAnonymously:failure", task.getException());
+                            Toast.makeText(Welcome.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+//    AccessToken accessToken = AccessToken.getCurrentAccessToken();
+//    boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
 
 
     private void handlerFacebookToken(AccessToken loginResult) {
@@ -213,7 +317,13 @@ public class Welcome extends AppCompatActivity {
 
     }
 
-    private void signIn() {
+    private void googlesignIn() {
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         //Intent signInIntent =Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
@@ -290,7 +400,6 @@ public class Welcome extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
         github = findViewById(R.id.github);
         phone=findViewById(R.id.phone);
-        play=findViewById(R.id.play);
         twitter=findViewById(R.id.twitter);
         yahoo=findViewById(R.id.yahoo);
         anonymous=findViewById(R.id.anonymous);
